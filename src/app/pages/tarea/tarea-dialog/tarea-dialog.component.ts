a62b2a0b204a0c890cad93762712b47356f9a830
Ybeth Chavez <ybth.92.15@gmail.com>
2021-04-17 03:01:57 -0500

    modificacion

---
+++ src/app/pages/tarea/tarea-dialog/tarea-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea-dialog',
+  templateUrl: './tarea-dialog.component.html',
+  styleUrls: ['./tarea-dialog.component.css']
+})
+export class TareaDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
